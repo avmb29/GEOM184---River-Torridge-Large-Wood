af268ca3ef9391eae9ee37bf17907edbbf2b0478
avmb29 <avmb29@gmail.com>
2025-11-20 17:17:14 +0000

    Add files via upload

---
+++ App.R
@@ -0,0 +1,35 @@
+#Load package ----
+library(shiny)
+library(leaflet)
+library(sf)
+library(raster)
+library (ggplot2)
+library(ggiraph)
+library(RColorBrewer)
+library(terra)
+library(leafem)
+
+
+options(shiny.maxRequestSize = 1000 * 1024^2)
+
+#Run global script containing all your relevant data ----
+source("Global.R")
+
+#Define UI for visualisation ----
+source("UI.R")
+
+ui <- navbarPage("Instream large wood on the River Torridge", id ="nav",
+                 tabPanel("Map",
+                          div(class ="outer",
+                              leafletOutput("map", height ="calc(100vh - 70px)")
+                              )
+                          )
+)
+
+#Define the server that performs all necessary operations ----
+server <- function(input, output, session){
+  source("Server.R", local = TRUE)
+}
+
+#Run the application ----
+shinyApp(ui, server)
